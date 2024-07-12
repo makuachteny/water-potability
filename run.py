ef04c80433b244ea05b07d37e4fa5fdda3dfb201
AvitBrian <113444617+AvitBrian@users.noreply.github.com>
2024-07-12 00:22:35 +0000

    added documentation

--- run.py
+++ run.py
@@ -5,6 +5,16 @@ import tensorflow as tf
 import numpy as np
 
 def predict_water_potability(url, model_path):
+    """
+    Predicts the potability of water based on the last row
+    of data from a local API endpoint.
+    Args:
+        url (str): The URL of the local API endpoint.
+        model_path (str): The file path of the pre-trained model.
+    Returns:
+        numpy.ndarray: The prediction result.
+    """
+    
     response = requests.get(url)
     df = pd.DataFrame(data)
     last_row = df.iloc[-1]
@@ -14,3 +24,5 @@ def predict_water_potability(url, model_path):
     last_row_values = np.expand_dims(last_row_values, axis=0)
     y_pred = model.predict(last_row_values)
     return y_pred
+
+print(predict_water_potability('http://127.0.0.1:5000/','./Models/potability_model.keras'))
